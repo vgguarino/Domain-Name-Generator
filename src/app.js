82e22941316d0b722cc64f58bda499b3bbe58333
vgguarino <victorgabrielguarino@gmail.com>
2022-11-17 08:03:42 +0000

    Domain Generator Complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
+
+console.log("Domain Generator 🌐");
+
+const pronoun = ["the", "our", "last"];
+
+const adj = ["great", "big", "of"];
+
+const noun = ["jogger", "racoon", "us"];
+
+const end = [".com", ".net", ".es", ".io", ".us"];
+
+let domain = [];
+
+pronoun.forEach(pronombre => {
+  adj.forEach(adjetivo => {
+    noun.forEach(nombre => {
+      end.forEach(tld => {
+        domain.push(pronombre + adjetivo + nombre + tld);
+      });
+    });
+  });
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function Generator() {
+  return (document.querySelector("#domaingenerator1").innerHTML = `
+  ${domain[getRandomInt(domain.length)]}`);
+}
+
+console.log(domain);
+//};
